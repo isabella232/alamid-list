3c8f6d2313a010377fe4889cf9467e7fc14be1a6
jhnns <johannes.ewald@peerigon.com>
2013-09-26 19:44:44 +0200

    - improved use() interface

--- test/List.test.js
+++ test/List.test.js
@@ -33,13 +33,26 @@ describe("List", function () {
     });
 
     describe(".use()", function () {
+        var plugin,
+            config;
 
-        it("should provide an plugin-interface", function () {
-            var plugin = sinon.spy();
+        beforeEach(function () {
+            plugin = sinon.spy();
+            config = {};
+        });
 
-            List.use(plugin);
-            expect(plugin).to.have.been.calledWith(List);
-            expect(List.use(plugin)).to.equal(List);
+        it("should provide a plugin-interface", function () {
+            List.use(plugin, config);
+            expect(plugin).to.have.been.calledWith(List, config);
+        });
+
+        it("should be usable on other objects too", function () {
+            var otherObj = {
+                use: List.use
+            };
+
+            otherObj.use(plugin);
+            expect(plugin).to.have.been.calledWith(otherObj);
         });
 
         it("should be chainable", function () {
