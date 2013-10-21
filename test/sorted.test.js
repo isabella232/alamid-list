f56250ad35d27620290c0e4a7a769de2d646a05d
Johannes Ewald <johannes.ewald@peerigon.com>
2013-10-22 00:43:01 +0200

    - improved test of sorted-plugin

--- test/sorted.test.js
+++ test/sorted.test.js
@@ -38,7 +38,7 @@ describe("plugins/sorted", function () {
         var event;
 
         List.prototype.config.emit = emit = sinon.spy();
-        list.push(2, 4, 5);
+        list.push(5, 2, 4);
 
         expect(emit).to.have.been.calledThrice;
 
@@ -47,30 +47,27 @@ describe("plugins/sorted", function () {
         expect(event).to.eql({
             type: "add",
             target: list,
-            element: 2,
+            element: 5,
             index: 0
         });
-        expect(event.target.toArray()[event.index]).to.equal(event.element);
 
         expect(emit.secondCall).to.have.been.calledWith("add");
         event = emit.secondCall.args[1];
         expect(event).to.eql({
             type: "add",
             target: list,
-            element: 4,
-            index: 1
+            element: 2,
+            index: 0 // 0 because element 2 is sorted before element 5
         });
-        expect(event.target.toArray()[event.index]).to.equal(event.element);
 
         expect(emit.thirdCall).to.have.been.calledWith("add");
         event = emit.thirdCall.args[1];
         expect(event).to.eql({
             type: "add",
             target: list,
-            element: 5,
-            index: 2
+            element: 4,
+            index: 1
         });
-        expect(event.target.toArray()[event.index]).to.equal(event.element);
 
         List.prototype.config.emit = emit = sinon.spy();
         list.pop();
